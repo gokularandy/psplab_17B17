dce235f8031f1deecc1cb4685db419c4e048362c
gokularandy <33417802+gokularandy@users.noreply.github.com>
2017-11-13 16:21:31 +0530

    Add files via upload

---
+++ insertsort.py.py
@@ -0,0 +1,16 @@
+
+def insertsort(L):
+
+  for index in range(1, len(L)):
+    key = L[index]
+    j = index - 1
+
+    while j >= 0 and (L[j] > key):
+      L[j + 1] = L[j]
+      j = j - 1
+    L[j + 1] = key
+  return L
+
+
+L = [5,-4,0,29,2]
+print(insertsort(L))
